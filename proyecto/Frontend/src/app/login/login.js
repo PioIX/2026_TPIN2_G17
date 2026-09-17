8ff2233f823c93db8783fe8dcfb5af25a934d2ff
lufa12343 <lfabiani@pioix.edu.ar>
2026-09-17 10:07:10 -0300

    tt

---
+++ proyecto/Frontend/src/app/login/login.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState, useEffect} from "react";
+
+export default function login(  {type, placeholder, value, onChange}){
+    return(
+        <div>
+            
+        </div>
+    )   
+
+}
---
+++ proyecto/Frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Input({ type, placeholder, value, onChange }) {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
